954202e1dff9c4bac12ced39ed92662fd1083c98
rajaboff007 <asadbekrajaboff1337@gmail.com>
2024-09-17 21:54:26 +0500

    asadbek commited

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
+    extend: {
+      colors: {
+        "mainColor": '#1C1E53',
+        "mainTextColor": '#282938'
+      }
+    },
+  },
+  plugins: [],
+}
+
